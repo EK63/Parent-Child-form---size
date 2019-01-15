8fb55752adcdcb621d5695f904489ff84c71efd5
EK63 <46166714+EK63@users.noreply.github.com>
2019-01-15 19:03:45 +0200

    Add files via upload

---
+++ PM1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PM1
+{
+    public partial class Form1 : Form
+    {
+        //
+        // Static code START
+        //
+            public static int mheight;
+            public static Form1 form1;
+            public Form1()
+            {
+                    InitializeComponent();
+                // assign the current object to form1
+                    form1 = this;
+            }
+
+            private void openToolStripMenuItem_Click_1(object sender, EventArgs e)
+            {
+                    Form2 newMDIChild = new Form2();
+                // Set the Parent Form of the Child window.  
+                    newMDIChild.MdiParent = this;
+                // Display the new form.  
+                    newMDIChild.Show();
+            }  
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+                // get menustrip's height
+                    mheight = this.menuStrip1.Height;
+            }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+       
+        //
+        // Static code END
+        //
+    }
+}
---
+++ PM1/Form2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PM1
+{
+    public partial class Form2 : Form
+    {
+        //
+        // Size code START
+        //
+            private void Form2_Load(object sender, EventArgs e)
+            {
+                string message6 = "Form 2 loaded";
+                    MessageBox.Show(message6);
+
+                // Form2 size start
+                    this.WindowState = FormWindowState.Normal;
+                    Size NewSize = new Size(Form1.form1.ClientRectangle.Width, Form1.form1.ClientRectangle.Height);
+                //Modifiy the size so any toolbars & taskbar can be easily accessed.
+                    NewSize.Height -= 75;
+                // minus mheight
+                    NewSize.Height -= Form1.mheight;
+                    NewSize.Width -= 12;
+                    this.Size = NewSize;
+                //Point NewLoc = Screen.FromControl(this).WorkingArea.Location;
+                    Point NewLoc = new Point();
+                //Modifiy the location so any toolbars & taskbar can be easily accessed.
+                    NewLoc.X += 12;
+                    NewLoc.Y += 75;
+                    this.Location = NewLoc;
+                //
+                    this.MinimumSize = this.Size;
+                    this.MaximumSize = this.MinimumSize;
+                // end
+            }
+        //
+        // Size code END
+        //
+    }
+}
